c3db0df3abe84ab64a2cdde89795b0220851c75b
Morten Mjelva <morten.mjelva@gmail.com>
2013-04-07 18:09:53 +0200

    partially finished messages

--- src/messages.c
+++ src/messages.c
@@ -1,44 +1,84 @@
 #include "messages.h"
 
+#include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
+#include <time.h>
 
 #include <cJSON/cJSON.h>
 
-char* messages_create_order(int floor, int type) {
+char*
+message_create (message_t msg) {
     char* out;
-    cJSON *root, *cli;
+    cJSON *root, *call_up, *call_down, *cmnds, *targets;
 
     root = cJSON_CreateObject();
-    cJSON_AddStringToObject(root, "test", "test");
-    cJSON_AddItemToObject(root, "elev_order", cli=cJSON_CreateObject());
-    cJSON_AddNumberToObject(cli, "order_type", type);
-    cJSON_AddNumberToObject(cli, "order_floor", floor);
+    cJSON_AddItemToObject(root, "call_up", call_up=cJSON_CreateObject());
+    cJSON_AddItemToObject(root, "call_down", call_down=cJSON_CreateObject());
+    cJSON_AddItemToObject(root, "commands", cmnds=cJSON_CreateObject());
+    cJSON_AddItemToObject(root, "targets", targets=cJSON_CreateObject());
 
+	for (int i = 0; i < (N_FLOORS-1) ; i++) {
+        cJSON *floor;
+
+        cJSON_AddItemToObject(call_up, "floor", floor=cJSON_CreateObject());
+        cJSON_AddNumberToObject(floor, "number", i);
+        cJSON_AddNumberToObject(floor, "time", callUp[i].timeRegistered);
+        cJSON_AddNumberToObject(floor, "registered", callUp[i].registered);
+        cJSON_AddNumberToObject(floor, "targeted", callUp[i].targeted);
+
+        cJSON_AddItemToObject(call_down, "floor", floor=cJSON_CreateObject());
+        cJSON_AddNumberToObject(floor, "number", i);
+        cJSON_AddNumberToObject(floor, "time", callDown[i].timeRegistered);
+        cJSON_AddNumberToObject(floor, "registered", callDown[i].registered);
+        cJSON_AddNumberToObject(floor, "targeted", callDown[i].targeted);
+    }
+
+	for (int i = 0; i < MAX_N_ELEVATORS; i++) {
+        cJSON *elev;
+        cJSON_AddItemToObject(cmnds, "elevator", elev=cJSON_CreateObject());
+        cJSON_AddNumberToObject(elev, "number", i);
+
+		for (int j = 0; j < N_FLOORS; j++) {
+            cJSON *floor;
+
+            cJSON_AddItemToObject(elev, "elevator", floor=cJSON_CreateObject());
+            cJSON_AddNumberToObject(floor, "number", i);
+            cJSON_AddNumberToObject(floor, "time", commands[i][j].timeRegistered);
+            cJSON_AddNumberToObject(floor, "targeted", commands[i][j].targeted);
+        }
+    }
+
+    cJSON_AddNumberToObject(root, "timestamp", time(NULL));
+  
     out = cJSON_Print(root);
     cJSON_Delete(root);
-
-
+      
     return out;
 }
 
-order_t messages_parse_order(char* msg) {
-    order_t order;
+message_t
+message_parse (char* msg) {
+    message_t message;
     char* tmp = malloc(strlen(msg));
     strcpy(tmp, msg); 
 
     cJSON* root = cJSON_Parse(tmp);
 
     if (root != 0) {
-      cJSON *elev_order = cJSON_GetObjectItem(root, "elev_order");
-      order.type = cJSON_GetObjectItem(elev_order, "order_type")->valueint;
-      order.floor = cJSON_GetObjectItem(elev_order, "order_floor")->valueint;
+        message.type = cJSON_GetObjectItem(root, "order_type")->valueint;
+        message.floor = cJSON_GetObjectItem(root, "order_floor")->valueint;
+        message.timestamp = cJSON_GetObjectItem(root, "order_floor")->valueint;
     }
 
     free(tmp);
     cJSON_Delete(root);
 
-    return order;
+    return message;
+}
+
+void
+message_print (message_t msg) {
+    printf("type: %d, order floor %d\n", msg.type, msg.floor);
 }
--- src/messages.h
+++ src/messages.h
@@ -1,12 +1,33 @@
 #ifndef __MESSAGES_H__
 #define __MESSAGES_H__
 
+#include <time.h>
+#include <stdbool.h>
+
+#include "orderlist.h"
+
+extern orderinfo_t callUp[ N_FLOORS-1 ]; 					// 0 = floor 1, N_FLOORS = floor N_FLOORS - 1.
+extern orderinfo_t callDown[ N_FLOORS-1 ];					// 0 = floor 2, N_FLOORS = floor N_FLOORS.
+extern orderinfo_t commands[ MAX_N_ELEVATORS ][ N_FLOORS ];
+extern int targets[MAX_N_ELEVATORS];
+
 typedef struct {
     int type;
 	int floor;
-} order_t;
+    time_t timestamp;
+} message_t;
 
-char* messages_create_order(int floor, int type);
-order_t messages_parse_order(char *msg);
+typedef enum {
+    HEARTBEAT,
+    ORDER_COMPLETE,
+    ORDER_REGISTERED,
+    N_ORDER_TYPES
+} order_type_t;
+
+
+char* message_create (message_t msg);
+message_t message_parse (char *msg);
+
+void message_print (message_t msg);
 
 #endif
